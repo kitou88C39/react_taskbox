198f2b15988d08f58b86a9e74b182f9eaf9f76ea
kitou88C39 <kanfutrooper@icloud.com>
2023-07-05 16:31:17 +0900

    .storybook/main.jsを編集

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,17 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+module.exports = {
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ['../public'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  features: {
+    postcss: false,
   },
-  docs: {
-    autodocs: "tag",
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack4',
   },
 };
-export default config;
